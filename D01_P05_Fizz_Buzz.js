f8b2b037392cf7ba52ed817e181da9aef0b445a5
utsab-ad <utasbadhikari075@gmail.com>
2025-05-01 10:17:23 -0600

    COnditional added in Fizz-Buzz

--- D01_P05_Fizz_Buzz.js
+++ D01_P05_Fizz_Buzz.js
@@ -11,14 +11,21 @@ return m%n === 0;
 }
 
 
-for (let i = 1; i <= noOf; i++) {
-    if (cond(i, 3) && cond(i, 5)) {
-        print("FizzBuzz");
-    } else if (cond(i, 3)) {
-        print("Fizz");
-    } else if (cond(i, 5)) {
-        print("Buzz");
-    } else {
-        console.log(i);
-    }
-}
+// for (let i = 1; i <= noOf; i++) {
+//     if (cond(i, 3) && cond(i, 5)) {
+//         print("FizzBuzz");
+//     } else if (cond(i, 3)) {
+//         print("Fizz");
+//     } else if (cond(i, 5)) {
+//         print("Buzz");
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// it can be also done with conditional: 
+
+for(let i=1;i<=noOf;i++)
+        {
+            (cond(i, 3) && cond(i, 5) ? print("FizzBuzz") : (cond(i, 3) ? print("Fizz") : (cond(i, 5)) ? print("Buzz"): console.log(i)));
+        }
