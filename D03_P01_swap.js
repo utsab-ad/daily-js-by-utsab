c8ba58be8e382a576e14ee4fa766d787f876b43f
utsab-ad <utasbadhikari075@gmail.com>
2025-05-19 04:14:34 -0600

    updated

---
+++ D03_P01_swap.js
@@ -0,0 +1,11 @@
+
+let a = 20;
+let b = 30;
+
+console.log("Before Swap: a = "+a+" and b = "+b);
+
+let temp = a;
+    a = b;
+    b = temp;
+
+console.log("After Swap: a = "+a+" and b = "+b);
---
+++ D03_P02_Factorial.js
@@ -0,0 +1,8 @@
+let num = 5;
+let fact = 1;
+
+for (let i=num; i>0; i--) {
+    fact = fact*i;
+}
+
+console.log("factorial of "+num+" is "+fact);
